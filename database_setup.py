4b55159e6d7eb2435cb851de4a3ab38c06e6934f
angelaramiz <angelaramiz22@gmail.com>
2024-06-27 01:20:45 -0600

    PF2

--- database_setup.py
+++ database_setup.py
@@ -32,7 +32,7 @@ c.execute('''CREATE TABLE IngredientesTotales (
 aleaciones = [
     ('Steel Ingot',), ('Bronze Ingot',), ('Brass Ingot',), ('Billon Ingot',), 
     ('Nickel Ingot',), ('Duralumin Ingot',), ('Gilded Iron',), ('Cobalt Ingot',), 
-    ('Ferrosilicon',), ('Lingote de latón de aluminio',), ('Lingote de bronce de aluminio',), 
+    ('Ferrosilicon',), ('Aluminium Brass Ingot',), ('Aluminium Bronze Ingot',), 
     ('Corinthian Bronze Ingot',), ('Damascus Steel Ingot',), ('Solder Ingot',), 
     ('Hardened Metal',), ('Redstone Alloy Ingot',), ('Reinforced Alloy Ingot',)
 ]
@@ -40,7 +40,7 @@ c.executemany('INSERT INTO Aleaciones (nombre) VALUES (?)', aleaciones)
 
 # Insertar datos de ejemplo en la tabla de ingredientes
 ingredientes = [
-    (1, 'Iron Dust', 2), (1, 'Carbon', 9),
+    (1, 'Iron Dust', 2), (1, 'Carbon', 1), (1, 'Iron Ingot',1),
     (2, 'Copper Dust', 1), (2, 'Tin Dust', 1), (2, 'Copper Ingot', 1),
     (3, 'Copper Dust', 1), (3, 'Zinc Dust', 1), (3, 'Copper Ingot', 1),
     (4, 'Silver Dust', 1), (4, 'Copper Dust', 1), (4, 'Silver Ingot', 1),
@@ -49,8 +49,8 @@ ingredientes = [
     (7, 'Gold Ingot (24-Carat)', 1), (7, 'Iron Dust', 1),
     (8, 'Iron Dust', 1), (8, 'Copper Dust', 1), (8, 'Nickel Ingot', 1),
     (9, 'Iron Ingot', 1), (9, 'Iron Dust', 1), (9, 'Silicon', 1),
-    (10, 'Polvo de aluminio', 1), (10, 'Lingote de latón', 1), (10, 'Lingote de aluminio', 1),
-    (11, 'Polvo de aluminio', 1), (11, 'Lingote de bronce', 1), (11, 'Lingote de aluminio', 1),
+    (10, 'Aluminium Dust', 1), (10, 'Brass ingot', 1), (10, 'Aluminium Ingot', 1),
+    (11, 'Aluminium Dust', 1), (11, 'Bronze Ingot', 1), (11, 'Aluminium Ingot', 1),
     (12, 'Silver Dust', 1), (12, 'Gold Dust', 1), (12, 'Copper Dust', 1), (12, 'Bronze Ingot', 1),
     (13, 'Steel Ingot', 1), (13, 'Iron Dust', 1), (13, 'Carbon', 1), (13, 'Iron Ingot', 1),
     (14, 'Lead Dust', 1), (14, 'Tin Dust', 1), (14, 'Lead Ingot', 1),
@@ -62,7 +62,7 @@ c.executemany('INSERT INTO Ingredientes (aleacion_id, ingrediente, cantidad) VAL
 
 # Insertar datos de ejemplo en la tabla de ingredientes totales
 ingredientes_totales = [
-    (1, 'Iron Dust', 2), (1, 'Carbon', 9),
+    (1, 'Iron Dust', 2), (1, 'Coal', 9),
     (2, 'Copper Dust', 2), (2, 'Tin Dust', 1),
     (3, 'Copper Dust', 2), (3, 'Zinc Dust', 1),
     (4, 'Silver Dust', 2), (4, 'Copper Dust', 1),
@@ -74,11 +74,11 @@ ingredientes_totales = [
     (10, 'Aluminum Dust', 2), (10, 'Copper Dust', 2), (10, 'Zinc Dust', 1),
     (11, 'Aluminum Dust', 2), (11, 'Copper Dust', 2), (11, 'Tin Dust', 1),
     (12, 'Silver Dust', 1), (12, 'Gold Dust', 1), (12, 'Copper Dust', 3), (12, 'Tin Dust', 1),
-    (13, 'Iron Dust', 4), (13, 'Carbon', 16),
+    (13, 'Iron Dust', 4), (13, 'Coal', 16),
     (14, 'Lead Dust', 2), (14, 'Tin Dust', 1),
     (15, 'Iron Dust', 4), (15, 'Aluminum Dust', 4), (15, 'Copper Dust', 3), (15, 'Tin Dust', 1), (15, 'Carbon', 48),
     (16, 'Redstone Dust', 10), (16, 'Iron Dust', 6), (16, 'Aluminum Dust', 4), (16, 'Copper Dust', 3), (16, 'Tin Dust', 1), (16, 'Block of Quartz', 1), (16, 'Carbon', 48),
-    (17, 'Iron Dust', 8), (17, 'Aluminum Dust', 4), (17, 'Copper Dust', 7), (17, 'Tin Dust', 3), (17, 'Silver Dust', 3), (17, 'Gold Dust', 12), (17, 'Lead Dust', 2), (17, 'Carbon', 64)
+    (17, 'Iron Dust', 8), (17, 'Aluminum Dust', 4), (17, 'Copper Dust', 7), (17, 'Tin Dust', 3), (17, 'Silver Dust', 3), (17, 'Gold Dust', 12), (17, 'Lead Dust', 2), (17, 'Coal', 64)
 ]
 c.executemany('INSERT INTO IngredientesTotales (aleacion_id, ingrediente, cantidad) VALUES (?, ?, ?)', ingredientes_totales)
 
